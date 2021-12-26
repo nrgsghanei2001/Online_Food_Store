2d943925648d0c507c816f0d3b7abac662d3047a
nargesghanei2001 <nargesghanei031@gmail.com>
2021-12-26 19:21:50 +0330

    change database to postgresql

--- final_project/settings.py
+++ final_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_food',
+        'USER': 'postgres',
+        'PASSWORD': 'naparmanam',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
--- online_food/models.py
+++ online_food/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import Address
 
 
 class Category(models.Model):
@@ -20,3 +21,19 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Branch(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='branches')
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name="branch")
+    # foods = models.ManyToManyField('Food', related_name='branches')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='branches')
+    details = models.TextField()
+    date_of_register = models.DateField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return f"{self.restaurant}'\n'branch: {self.address}"
+
+
+# class Food(models.Model):
