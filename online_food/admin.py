2e260d36cf71e8c3422716af69d32390d0142130
nargesghanei2001 <nargesghanei031@gmail.com>
2021-12-26 18:26:39 +0330

    create Category model for foods and restaurants

--- online_food/admin.py
+++ online_food/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.db.models.fields import CharField
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Category)
--- online_food/models.py
+++ online_food/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 
+class Category(models.Model):
+    MEAL_CHOICES = (('bf', 'Break Fast'),
+    ('ld', 'Lunch And Dinner'),
+    ('sn', 'Snack'))
+
+    name = models.CharField(max_length=50)
+    meal = models.CharField(max_length=2, choices=MEAL_CHOICES, default='ld')
+
+
+    def __str__(self):
+        return f"{self.name} {self.get_meal_display()}"
