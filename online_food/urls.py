f9602e980f0d9709f47311ea70be31e2239e1d69
nargesghanei2001 <nargesghanei031@gmail.com>
2022-01-02 22:07:30 +0330

    add urls and templates for show restaurants and their menu

--- online_food/urls.py
+++ online_food/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import *
-
+from django.contrib.staticfiles.urls import static
+from final_project import settings
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('add/food/', add_food, name='add_food'),
+    path('all/restaurants/', AllRestaurants.as_view(), name='all_restaurants'),
+    path('all/restaurants/<int:pk>', RestaurantDetail.as_view(), name='restaurants_detail'),
+    path('all/restaurants/menu/<int:pk>', Menu.as_view(), name='restaurants_menu'),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- online_food/views.py
+++ online_food/views.py
@@ -1,5 +1,28 @@
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+
+from online_food.models import Branch
 
 
 def home_page(request):
     return render(request, 'online_food/home.html')
+
+
+def add_food(request):
+    return render(request, 'online_food/add_food.html')
+
+
+class AllRestaurants(ListView):
+    model = Branch
+    template_name = 'online_food/all_restaurants.html' 
+
+
+class RestaurantDetail(DetailView):
+    model = Branch
+    template_name = 'online_food/restaurants_detail.html' 
+
+
+class Menu(DetailView):
+    model = Branch
+    template_name = 'online_food/restaurants_menu.html' 
