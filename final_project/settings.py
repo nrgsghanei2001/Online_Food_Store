8085997eda3ebca571ab6735a27a8cb55e21ec09
nargesghanei2001 <nargesghanei031@gmail.com>
2022-01-05 18:54:27 +0330

    debug show images part and fix it

--- final_project/settings.py
+++ final_project/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/online_food_medias/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- online_food/urls.py
+++ online_food/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path('', home_page, name='home'),
@@ -13,6 +14,8 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT + os.path.altsep)
 
 
